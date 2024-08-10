9fa7841e9a8bcff09b95b395f1225e61247310c3
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-08-10 10:33:28 +0100

    feat: add docs for ex09

--- module-00/ex09/my_package/__init__.py
+++ module-00/ex09/my_package/__init__.py
@@ -1,2 +1,11 @@
 def count_in_list(arr: list[str], str: str) -> str:
+    """this function counts the number of times a string appears in a list
+
+    Args:
+        arr (list[str]): list of strings
+        str (str): string to count
+
+    Returns:
+        str: number of times the string appears in the list
+    """
     return arr.count(str)
