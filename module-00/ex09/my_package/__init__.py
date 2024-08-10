a19c86b6220671713a3313ba7d66eba819ce2e64
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-08-10 10:19:23 +0100

    feat: :sparkles: ex09 done

---
+++ module-00/ex09/my_package/__init__.py
@@ -0,0 +1,2 @@
+def count_in_list(arr: list[str], str: str) -> str:
+    return arr.count(str)
---
+++ module-00/ex09/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ft_package',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A sample test package',
+    author='mbenkhat',
+    author_email='mbenkhat@student.1337.ma',
+    url='https://github.com/1337Impact',
+    license='MIT',
+    metadata_version='2.1',
+    Installer='pip',
+    install_requires=[],
+)
